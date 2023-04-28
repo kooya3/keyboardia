97881b166c2f5b301d6d58059755b94b6d500abf
kooya3 <elyeestatua@gmail.com>
2023-04-28 15:00:17 +0300

    created last edit

---
+++ main.js
@@ -0,0 +1,143 @@
+const keys = [..."ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+
+const timestamps = [];
+
+timestamps.unshift(getTimestamp());
+
+function getRandomNumber(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRandomKey() {
+  return keys[getRandomNumber(0, keys.length-1)]
+}
+
+function targetRandomKey() {
+  const key = document.getElementById(getRandomKey());
+  key.classList.add("selected");
+  let start = Date.now()
+}
+
+function getTimestamp() {
+  return Math.floor(Date.now() / 1000)
+}
+
+document.addEventListener("keyup", event => {
+  const keyPressed = String.fromCharCode(event.keyCode);
+  const keyElement = document.getElementById(keyPressed);
+  const highlightedKey = document.querySelector(".selected");
+  
+  keyElement.classList.add("hit")
+  keyElement.addEventListener('animationend', () => {
+    keyElement.classList.remove("hit")
+  })
+  
+  if (keyPressed === highlightedKey.innerHTML) {
+    timestamps.unshift(getTimestamp());
+    const elapsedTime = timestamps[0] - timestamps[1];
+    console.log(`Character per minute ${60/elapsedTime}`)
+
+    highlightedKey.classList.remove("selected");
+    targetRandomKey();
+  } 
+})
+
+targetRandomKey();
+
+const elts = {
+	text1: document.getElementById("text1"),
+	text2: document.getElementById("text2")
+};
+
+// The strings to morph between. You can change these to anything you want!
+const texts = [
+/* 	"I",
+	"am",
+	"very",
+	"sorry",
+	"baby",
+	"I",
+	"love u",
+  "I know",
+  "that I let you down",
+  "that I'm not there for you",
+  "asanti for everything", */
+];
+
+// Controls the speed of morphing.
+const morphTime = 1;
+const cooldownTime = 0.25;
+
+let textIndex = texts.length - 1;
+let time = new Date();
+let morph = 0;
+let cooldown = cooldownTime;
+
+elts.text1.textContent = texts[textIndex % texts.length];
+elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+
+function doMorph() {
+	morph -= cooldown;
+	cooldown = 0;
+	
+	let fraction = morph / morphTime;
+	
+	if (fraction > 1) {
+		cooldown = cooldownTime;
+		fraction = 1;
+	}
+	
+	setMorph(fraction);
+}
+
+// A lot of the magic happens here, this is what applies the blur filter to the text.
+function setMorph(fraction) {
+	// fraction = Math.cos(fraction * Math.PI) / -2 + .5;
+	
+	elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+	elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+	
+	fraction = 1 - fraction;
+	elts.text1.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+	elts.text1.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+	
+	elts.text1.textContent = texts[textIndex % texts.length];
+	elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+}
+
+function doCooldown() {
+	morph = 0;
+	
+	elts.text2.style.filter = "";
+	elts.text2.style.opacity = "100%";
+	
+	elts.text1.style.filter = "";
+	elts.text1.style.opacity = "0%";
+}
+
+// Animation loop, which is called every frame.
+function animate() {
+	requestAnimationFrame(animate);
+	
+	let newTime = new Date();
+	let shouldIncrementIndex = cooldown > 0;
+	let dt = (newTime - time) / 1000;
+	time = newTime;
+	
+	cooldown -= dt;
+	
+	if (cooldown <= 0) {
+		if (shouldIncrementIndex) {
+			textIndex++;
+		}
+		
+		doMorph();
+	} else {
+		doCooldown();
+	}
+}
+
+// Start the animation.
+animate();
